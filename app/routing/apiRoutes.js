4fdf12ff765656938b730422ab0b2d1fac698d6c
Manuel <manuelantoniopadilla@gmail.com>
2018-11-11 23:43:11 -0600

    updated apiRoutes.js logic

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,5 +1,5 @@
 // require in data from friends.js
-var friends = require("../data/friends.js");
+let friends = require("../data/friends.js");
 
 // build route to view all of friends
 module.exports = function (app) {
@@ -24,19 +24,25 @@ module.exports = function (app) {
 
     console.log(userScores);
 
+    // difference between user and user's scores in the database is calculated
     let totalDifference = 0;
 
-    for (let i = 0; i < friends.length; i++) 
+    // Nested for loop, loop through friend possibilites in database
+    for (let i = 0; i < friends.length; i++) {
 
       console.log(friends[i]);
       totalDifference = 0;
 
+      // loop through all scores of each friend
       for (let j = 0; j < friends[i].scores[j]; j++) {
 
+        // totalDifference takes the sum of the difference calculated between the scores
         totalDifference += Math.abs(parseInt(userScores[j]) - parseInt(friends[i].scores[j]));
 
+        // if the sum of totalDifferences is less then the differences of the current "best match"
         if (totalDifference <= bestMatch.friendDifference) {
 
+          // reset the bestMatch to be the new best friend
           bestMatch.name = friends[i].name;
           bestMatch.photo = friends[i].photo;
           bestMatch.friendDifference = totalDifference;
@@ -44,8 +50,10 @@ module.exports = function (app) {
       }
     }
 
+    // user's data is then saved to the database.
     friends.push(userData);
 
+    // return new bestMatch object in JSON format back to frontend app
     res.json(bestMatch);
   });
 
