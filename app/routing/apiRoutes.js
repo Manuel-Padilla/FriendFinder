55f1b11368e67c34f90cbf39e0607be1e504c83f
Manuel <manuelantoniopadilla@gmail.com>
2018-11-14 12:14:40 -0600

    changed port on readme.mdk

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,54 +1,47 @@
 // require in data from friends.js
-var friends = require('../data/friends.js');
+var friendsData = require('../data/friends.js');
 
 // build route to view all of friends
 module.exports = function (app) {
 
   // display all friends
   app.get("/api/friends", function (req, res) {
-    res.json(friends);
+    res.json(friendsData);
   });
 
   app.post("/api/friends", function (req, res) {
 
-    let bestMatch = {
-      name: "",
+    var newFriend = req.body;
+    var newFriendResponses = newFriend.scores;
+    var bestMatch = {
+      name: "FRIEND",
       photo: "",
-      friendDifference: 100
+      bestMatchDifference: 500
     };
 
-    console.log(req.body);
-
-    // result of user's survey POST and parse it
-    let userData = req.body;
-    let userScores = userData.scores;
-
-    console.log(userScores);
-
-    // difference between user and user's scores in the database is calculated
-    let totalDifference = 0;
 
     // Nested for loop, loop through friend possibilites in database
-    for (var i = 0; i < friends.length; i++) {
+    for (var i = 0; i < friendsData.length; i++) {
 
-      console.log(friends[i]);
-      totalDifference = 0;
+      var diffCounter = 0;
 
       // loop through all scores of each friend
-      for (var j = 0; j < friends[i].scores[j]; j++) {
+      for (var j = 0; j < friendsData[i].scores.length; j++) {
 
         // totalDifference takes the sum of the difference calculated between the scores
-        totalDifference += Math.abs(parseInt(userScores[j]) - parseInt(friends[i].scores[j]));
+        diffCounter += Math.abs(parseInt(friendsData[i].scores[j]) - parseInt(newFriendsResponses[j]))
+
+      }
 
         // if the sum of totalDifferences is less then the differences of the current "best match"
-        if (totalDifference <= bestMatch.friendDifference) {
+        if (bestMatch.bestMatchDifference > diffCounter) {
 
           // reset the bestMatch to be the new best friend
-          bestMatch.name = friends[i].name;
-          bestMatch.photo = friends[i].photo;
-          bestMatch.friendDifference = totalDifference;
+          bestMatch.name = friendsData[i].name;
+          bestMatch.photo = friendsData[i].photo;
+          bestMatch.bestMatchDifference = diffCounter;
         }
-      }
+      
     }
 
     // user's data is then saved to the database.
